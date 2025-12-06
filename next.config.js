1d56ef69190a1923246f5f088772ff798ecc2163
SADMAN30102001SAKIB <evan30102001@gmail.com>
2025-12-07 03:18:21 +0600

    Add initial wrangler configuration for portfolio project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com'],
+    unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
